d2315d6e2ac7041879f4a0fc5a00e8a21cc5537b
AKD <contact.mousom@gmail.com>
2023-12-10 23:22:32 +0600

    Portfolio website initial setup is done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Mainlayout from './Layout/MainLayout/Mainlayout'
+
+function App() {
+
+
+  return (
+    <>
+     <Mainlayout></Mainlayout>
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/MainLayout/Mainlayout.jsx
@@ -0,0 +1,12 @@
+
+
+const Mainlayout = ({children}) => {
+    return (
+        <div>
+            Hello
+            {children}
+        </div>
+    );
+};
+
+export default Mainlayout;
