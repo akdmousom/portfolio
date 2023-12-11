8247c65b3f4de539e86cf521e36d7613cc4d2256
AKD <contact.mousom@gmail.com>
2023-12-11 13:50:06 +0600

    My image added done

--- src/Layout/MainLayout/Mainlayout.jsx
+++ src/Layout/MainLayout/Mainlayout.jsx
@@ -6,7 +6,9 @@ import Navigation from '../../Components/Shared/Navigation/Navigation';
 const Mainlayout = ({children}) => {
     return (
         <div>
+          <div className='max-w-[1200px] mx-auto'>
           <Navigation/>
+          </div>
            
             {children}
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,24 @@
 import Particle from "../../Components/Particle/Particle";
+import Arijit from "../../assets/Imeges/arijit.png"
 
 const Home = () => {
     return (
         <div>
-               <div className="z-[-10] bg-gradient-to-r from-[#c2e59c] to-[#64b3f4]  absolute w-full top-[-10px]">
-               <Particle/>
-               <div className=" absolute bottom-2/4 w-full mx-auto text-center">
-                <h1>Welcome to my profile</h1>
-                
+            <div className="z-[-10] bg-gradient-to-r from-[#c2e59c] to-[#64b3f4]  absolute w-full top-[-10px]">
+                <Particle />
+                <div className=" absolute bottom-2/4 w-full mx-auto text-center">
+                    <h1>Welcome to my profile</h1>
+
                 </div>
-          
-               </div>
-            
+
+            </div> 
+
+            <div className="grid justify-center lg:justify-normal max-w-[1200px] mx-auto">
+                <div className="rounded-full relative top-[220px]  ">
+                    <img className="h-[200px] w-[200px] rounded-full object-cover"  src={Arijit} alt="" />
+                </div>
+            </div>
+
         </div>
     );
 };
